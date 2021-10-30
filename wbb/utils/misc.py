63aa6846134eb5605f38b82684ffc048a87939c6
botsiamrenga <92196968+botsiamrenga@users.noreply.github.com>
2021-10-30 12:02:29 +0530

    Update misc.py

--- wbb/utils/misc.py
+++ wbb/utils/misc.py
@@ -65,22 +65,22 @@ def paginate_modules(page_n, module_dict, prefix, chat=None):
             ]
         )
 
-    pairs = list(zip(modules[::8], modules[4::8], modules[6::8]))
-    i = 4
+    pairs = list(zip(modules[::3], modules[1::3], modules[2::3]))
+    i = 0
     for m in pairs:
         for _ in m:
-            i += 4
-    if len(modules) - i == 4:
-        pairs.append((modules[1],))
-    elif len(modules) - i == 6:
+            i += 1
+    if len(modules) - i == 1:
+        pairs.append((modules[-1],))
+    elif len(modules) - i == 2:
         pairs.append(
             (
-                modules[6],
-                modules[4],
+                modules[-2],
+                modules[-1],
             )
         )
 
-    COLUMN_SIZE = 8
+    COLUMN_SIZE = 4
 
     max_num_pages = ceil(len(pairs) / COLUMN_SIZE)
     modulo_page = page_n % max_num_pages
