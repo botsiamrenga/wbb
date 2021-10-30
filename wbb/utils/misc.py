77af7e6925cf5a1d2967e257766ad93baea68b64
botsiamrenga <92196968+botsiamrenga@users.noreply.github.com>
2021-10-30 15:41:12 +0530

    Update misc.py

--- wbb/utils/misc.py
+++ wbb/utils/misc.py
@@ -80,7 +80,7 @@ def paginate_modules(page_n, module_dict, prefix, chat=None):
             )
         )
 
-    COLUMN_SIZE = 12
+    COLUMN_SIZE = 15
 
     max_num_pages = ceil(len(pairs) / COLUMN_SIZE)
     modulo_page = page_n % max_num_pages
