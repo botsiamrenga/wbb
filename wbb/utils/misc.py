d93acfaf01ea06a0df3065c988399eb7e1a9ec5d
botsiamrenga <92196968+botsiamrenga@users.noreply.github.com>
2021-10-30 22:29:53 +0530

    Update misc.py

--- wbb/utils/misc.py
+++ wbb/utils/misc.py
@@ -1,18 +1,14 @@
 """
 MIT License
-
 Copyright (c) 2021 TheHamkerCat
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -65,8 +61,8 @@ def paginate_modules(page_n, module_dict, prefix, chat=None):
             ]
         )
 
-    pairs = list(zip(modules[::4], modules[-1::4], modules[-2::4]))
-    i = 4
+    pairs = list(zip(modules[::3], modules[1::3], modules[2::3]))
+    i = 0
     for m in pairs:
         for _ in m:
             i += 1
@@ -96,7 +92,7 @@ def paginate_modules(page_n, module_dict, prefix, chat=None):
                     callback_data="{}_prev({})".format(prefix, modulo_page),
                 ),
                 EqInlineKeyboardButton(
-                    "🔙 Back",
+                    "Back",
                     callback_data="{}_home({})".format(prefix, modulo_page),
                 ),
                 EqInlineKeyboardButton(
@@ -105,9 +101,6 @@ def paginate_modules(page_n, module_dict, prefix, chat=None):
                 ),
             )
         ]
-
-    else:
-        pairs += [[EqInlineKeyboardButton("「 GO BACK TO MAIN MENU 」", callback_data="help_back")]]
 
     return pairs
 
