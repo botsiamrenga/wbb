2f043659ec0bdd88739662dd41dbcf908cb94b05
botsiamrenga <92196968+botsiamrenga@users.noreply.github.com>
2021-10-30 10:39:14 +0530

    Update misc.py

--- wbb/utils/misc.py
+++ wbb/utils/misc.py
@@ -65,7 +65,7 @@ def paginate_modules(page_n, module_dict, prefix, chat=None):
             ]
         )
 
-    pairs = list(zip(modules[::3], modules[1::3], modules[2::3]))
+    pairs = list(zip(modules[::4], modules[1::4], modules[2::4]))
     i = 0
     for m in pairs:
         for _ in m:
