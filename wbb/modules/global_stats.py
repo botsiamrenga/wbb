7cba4160bae34e1112426145ee52999538b86ebf
botsiamrenga <92196968+botsiamrenga@users.noreply.github.com>
2021-10-31 08:04:05 +0530

    Update global_stats.py

--- wbb/modules/global_stats.py
+++ wbb/modules/global_stats.py
@@ -103,7 +103,7 @@ async def global_stats(_, message):
 
     # Contributors/Developers count and commits on github
     url = "https://api.github.com/repos/thehamkercat/williambutcherbot/contributors"
-    rurl = "https://github.com/thehamkercat/williambutcherbot"
+    rurl = "https://github.com/didikteatochhawng"
     developers = await get(url)
     commits = 0
     for developer in developers:
