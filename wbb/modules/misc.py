b679ed19cd83edc550cce046f59857442f195fe4
botsiamrenga <92196968+botsiamrenga@users.noreply.github.com>
2021-11-02 15:44:13 +0530

    Update misc.py

--- wbb/modules/misc.py
+++ wbb/modules/misc.py
@@ -34,7 +34,7 @@ from wbb.utils.http import get
 from wbb.utils.json_prettify import json_prettify
 from wbb.utils.pastebin import paste
 
-__MODULE__ = "Misc"
+__MODULE__ = "Lynn"
 __HELP__ = """
 /Lynn
     Ask a question
